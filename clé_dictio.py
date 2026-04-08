fb1ac762f70dc09710b3af4b87ab9c5c302ac137
Noa Jeanne-Cachot <noacachotjeanne@gmail.com>
2026-04-08 11:45:16 +0200

    ajout_dictio

---
+++ clé_dictio.py
@@ -0,0 +1,11 @@
+#décalarations
+
+mon_dictio ={
+    "nom" : "alices",
+    "age" : 22,
+    "ville" : "Paris",
+    "département" : 78
+}
+
+mon_dictio["age"] = "23 \U000026A0 modifier"
+print(mon_dictio["age"])
