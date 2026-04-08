2978ad795df30698d7dcafdd9e0fb977358dead2
Noa Jeanne-Cachot <noacachotjeanne@gmail.com>
2026-04-08 10:07:10 +0200

    mise a jour tableauMulti eleves

--- TpTableaux.py
+++ TpTableaux.py
@@ -1,18 +1,22 @@
 # Déclaration 
 
-eleve1 = [14, 7, 8, 19]
-eleve2 = [13, 9, 2, 10]
-eleve3 = [8, 9, 10, 18]
+eleve1 = [12, 14, 8, 7]
+eleve2 = [13, 18, 18, 6]
+eleve3 = [17, 12, 18, 2]
 
-eleves = [eleve1, 
-         eleve2, 
-         eleve3]
+eleves = [eleve1,
+          eleve2, 
+          eleve3]
 
-# Déclaration
-
+# Traitement 
 
 for i, eleve in enumerate(eleves):
     notes = ""
-    moyenne += i  
+    moyenne = 0
     for j, note in enumerate(eleve):
-        eleve
+        moyenne += note
+        notes += str(note) + " "
+    moyenne = moyenne / (j + 1)
+    print(f"Eleve {i+1} : {notes} ({moyenne:.2f}/20)")
+
+
